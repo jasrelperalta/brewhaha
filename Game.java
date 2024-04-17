c38a9648ec774ed9bb8834e60a19a25d0fd15a95
jasrelperalta <jtperalta1@up.edu.ph>
2024-04-17 22:59:46 +0800

    added images for mainmenu

--- Game.java
+++ Game.java
@@ -2,21 +2,26 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Game extends JFrame {
 
-    private final int WIDTH = 600;
-    private final int HEIGHT = 800;
+public class Game extends JFrame{
+
+    private final int WIDTH = 1000;
+    private final int HEIGHT = 600;
 
     public Game() {
-        setTitle("2D Game");
+        setTitle("BREW-HAHA!");
         setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setResizable(false);
+        setResizable(true);
 
         // Add game panel
         GamePanel gamePanel = new GamePanel();
         add(gamePanel);
+
+        // Display the main menu component
+
+
 
         setVisible(true);
     }
@@ -47,6 +52,18 @@ class GamePanel extends JPanel implements ActionListener {
         super.paintComponent(g);
 
         // Draw game objects and graphics here
+        ImageIcon img = new ImageIcon("img/bg.png");
+        g.drawImage(img.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
+        ImageIcon title = new ImageIcon("img/title.png");
+        g.drawImage(title.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
+        ImageIcon play = new ImageIcon("img/play.png");
+        g.drawImage(play.getImage(), 10, 0, this.getWidth(), this.getHeight(), null);
+        ImageIcon multi = new ImageIcon("img/multiplayer.png");
+        g.drawImage(multi.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
+        ImageIcon quit = new ImageIcon("img/quit.png");
+        g.drawImage(quit.getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
+
+
 
         Toolkit.getDefaultToolkit().sync();
     }
